23c486ae666085bde57d5043c719c8725bc17c5a
Tusharvatsa02 <83219333+Tusharvatsa02@users.noreply.github.com>
2022-02-02 19:17:23 +0530

    Add files via upload

---
+++ springboot-thymleaf-webapp/src/main/java/com/prog/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.prog.service;
+
+import java.util.List;
+
+import com.prog.model.Employee;
+
+public interface EmployeeService {
+	List < Employee > getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+}
